6b1ff8ed98e62e64597ed7c62170117e13c534f8
Mark Wang <Mark.Wang@quest.com>
2020-06-12 11:31:05 +0800

    Add a hello world CS file

---
+++ GitHello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GitHelloTest
+{
+    class GitHello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world...");
+            Console.ReadLine();
+        }
+    }
+}
